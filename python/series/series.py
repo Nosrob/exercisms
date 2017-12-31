000f6fce7c7553561fd959513703607655bd6df1
Nosrob <contact.nosrob@gmail.com>
2017-12-31 13:36:08 +0100

    Refactored Series

--- python/series/series.py
+++ python/series/series.py
@@ -1,7 +1,3 @@
 def slices(series, length):
-    res = []
     if len(series) < length or length == 0: raise ValueError("Incorrect input")
-    for i,v in enumerate(series):
-        if len(series) - i < length: break
-        res.append(list(map(int,series[i:i+length:1])))
-    return res
+    return [list(map(int, series[i:i+length:1])) for i in range(len(series) - length + 1)]
