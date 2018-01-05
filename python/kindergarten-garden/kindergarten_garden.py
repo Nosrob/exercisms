687036f0ae4629dcac37aed1f01153a44d4aa03f
[a511735] nicolas borson <nicolas.borson@atos.net>
2018-01-05 14:36:22 +0100

    refactor kindergarten

--- python/kindergarten-garden/kindergarten_garden.py
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -1,11 +1,10 @@
-students = ["Alice","Bob","Charlie","David","Eve","Fred","Ginny","Harriet","Ileana","Joseph","Kincaid","Larry"]
-plants = {"G": "Grass", "C": "Clover", "R": "Radishes", "V": "Violets"}
-
 class Garden(object):
-    def __init__(self, diagram, students=students):
+
+    def __init__(self, diagram, students = ["Alice","Bob","Charlie","David","Eve","Fred","Ginny","Harriet","Ileana","Joseph","Kincaid","Larry"]):
         self.diagram = diagram
-        self.students = sorted(students)
+        self.students  = sorted(students)
+        self.plantCodes = {"G": "Grass", "C": "Clover", "R": "Radishes", "V": "Violets"}
 
     def plants(self, student):
         index = self.students.index(student) * 2
-        return [plants[i] for i in [c for row in self.diagram.splitlines() for c in row[index:index+2:]]]
+        return [self.plantCodes[i] for i in [c for row in self.diagram.splitlines() for c in row[index:index+2:]]]
