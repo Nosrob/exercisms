c954c3b9612fe1a755cf185dfce46f81d5bfb1b6
[a511735] nicolas borson <nicolas.borson@atos.net>
2018-01-05 14:39:02 +0100

    Refactoring on Kindergarten

--- python/kindergarten-garden/kindergarten_garden.py
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -1,9 +1,10 @@
 class Garden(object):
-
-    def __init__(self, diagram, students = ["Alice","Bob","Charlie","David","Eve","Fred","Ginny","Harriet","Ileana","Joseph","Kincaid","Larry"]):
+    studentsList = ["Alice","Bob","Charlie","David","Eve","Fred","Ginny","Harriet","Ileana","Joseph","Kincaid","Larry"]
+    plantCodes = {"G": "Grass", "C": "Clover", "R": "Radishes", "V": "Violets"}
+    
+    def __init__(self, diagram, students = studentsList):
         self.diagram = diagram
         self.students  = sorted(students)
-        self.plantCodes = {"G": "Grass", "C": "Clover", "R": "Radishes", "V": "Violets"}
 
     def plants(self, student):
         index = self.students.index(student) * 2
